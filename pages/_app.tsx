940c355c23e172fa2c34fa0c4485c123b7fedcf8
Kartik Dhawan <dhawankartik7@gmail.com>
2022-12-03 01:39:03 +0530

    CW-07: Added layout, conditional layout & responsive styling mixins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import type { AppProps } from "next/app";
 import "../styles/index.scss";
+import Layout from "../components/common/Layout";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  console.log(router);
+
+  if (router.pathname === "/_error" || router.pathname === "/") {
+    return <Component {...pageProps} />;
+  } else {
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
 }
