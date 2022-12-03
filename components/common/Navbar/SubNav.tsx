b1cf5b1a7d7834df128636da49ab304486906fd0
Kartik Dhawan <dhawankartik7@gmail.com>
2022-12-03 19:14:45 +0530

    CW-08: Installed MUI. Added Nav & SubNav component (UI)

---
+++ components/common/Navbar/SubNav.tsx
@@ -0,0 +1,52 @@
+import { Button, Grid } from "@mui/material"
+import { styles } from ".././styles"
+
+const SubNav = () => {
+  const snid = "subNav"
+
+  return (
+    <Grid
+      container
+      sx={{
+        ...styles.nav,
+        margin: "10px 0px",
+      }}
+      className={snid + "Wrapper"}
+      id={snid + "Wrapper"}
+    >
+      <Grid
+        item
+        xs={6}
+        sm={6}
+        className={snid + "Title"}
+        id={snid + "Title"}
+        sx={styles.subNavTitle}
+      >
+        Permutations<br></br>with colors<br></br>and more.
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        sm={3}
+        textAlign={"right"}
+        sx={{ color: "#f4eebd" }}
+        className={snid + "ChooseTheme"}
+        id={snid + "ChooseTheme"}
+      >
+        Theme
+      </Grid>
+      <Grid item xs={0} sm={3} textAlign={"right"}>
+        <Button
+          variant="contained"
+          className={snid + "GetStartBtn"}
+          id={snid + "GetStartBtn"}
+          sx={styles.subNavButton}
+        >
+          Get Started
+        </Button>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default SubNav
---
+++ components/common/styles.ts
@@ -0,0 +1,67 @@
+export const styles = {
+  // nav
+  nav: {
+    padding: {
+      xs: "7px 4px",
+      sm: "10px 4px",
+      md: "12px 4px",
+      xl: "15px 4px",
+    },
+    textTransform: "uppercase",
+    fontWeight: 300,
+    letterSpacing: {
+      xs: "0px",
+      sm: "1px",
+    },
+    fontSize: {
+      xs: "14px",
+      md: "18px",
+      xl: "23px",
+    },
+  },
+  // subnav title
+  subNavTitle: {
+    fontSize: {
+      xs: "18px",
+      sm: "30px",
+      lg: "34px",
+      xl: "40px",
+    },
+    fontWeight: {
+      xs: 500,
+      sm: 300,
+    },
+    color: "#D9D9D9",
+  },
+  // subnav button
+  subNavButton: {
+    backgroundColor: "#D9D9D9",
+    color: "#111",
+    fontWeight: {
+      xs: 300,
+      sm: 500,
+    },
+    textTransform: "capitalize",
+    borderRadius: "10000px",
+    "&:hover": {
+      backgroundColor: "#D9D9D9",
+    },
+    letterSpacing: "0px",
+    fontSize: {
+      xs: "12px",
+      sm: "14px",
+      md: "18px",
+      xl: "22px",
+    },
+    padding: {
+      xs: "4px 14px",
+      sm: "7px 15px",
+      md: "10px 23px",
+      xl: "13px 30px",
+    },
+    margin: {
+      xs: "14px 0px",
+      sm: "0px 0px",
+    },
+  },
+}
