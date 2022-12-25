09dcd0771ebb50e24f8d79a17efac216d9d569b7
Kartik Dhawan <dhawankartik7@gmail.com>
2022-12-25 22:41:01 +0530

    CW-55: Added get-new-palette on hitting space functionality

--- components/PaletteSection/PaletteBar.tsx
+++ components/PaletteSection/PaletteBar.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material"
 import { styles } from "./styles/styles"
+import { useId } from "react"
 
 interface PaletteBarType {
   pid: string
@@ -7,9 +8,11 @@ interface PaletteBarType {
 }
 
 const PaletteBar = ({ pid, hexcode }: PaletteBarType) => {
+  const id = useId()
   return (
     <Box
       className={pid + "Bar"}
+      id={id + pid + "Bar"}
       sx={{
         ...styles.paletteSectionBar,
         backgroundColor: `#${hexcode}`,
