dfdd3eb76ec4cdb8701521e693dcbdbcc6a9bc67
Kartik Dhawan <dhawankartik7@gmail.com>
2022-12-29 02:12:34 +0530

    CW-68: Modified ErrorBoundary fallback UI

---
+++ components/common/ErrorFallback/styles.ts
@@ -0,0 +1,25 @@
+export const styles = {
+  errorFallbackWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+    gap: "20px",
+  },
+  errorFallbackHeaderAlert: {
+    width: "80%",
+    marginTop: "2.5rem",
+  },
+  errorFallbackTryAgainBtnWrapper: {
+    display: "flex",
+    justifyContent: "flex-start",
+    width: "80%",
+  },
+  errorFallbackTryAgainBtn: {
+    color: "#5f2120",
+    backgroundColor: "#fcedec",
+    "&:hover": {
+      backgroundColor: "#fcedec",
+    },
+  },
+}
