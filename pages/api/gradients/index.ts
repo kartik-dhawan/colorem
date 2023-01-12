13b038e64ce6489be16814894147828d5ca41b54
Kartik Dhawan <dhawankartik7@gmail.com>
2023-01-12 23:08:24 +0530

    CW-101: Created get-all-gradients API

--- pages/api/gradients/index.ts
+++ pages/api/gradients/index.ts
@@ -1,11 +1,20 @@
 import connectDatabase from "../../../lib/database/connect"
 import { NextApiRequest, NextApiResponse } from "next"
-import colorGradient from "../../../lib/database/models/colorGradient"
+import { getAllColorGradients } from "../../../lib/methods/gradients/getGradients"
+import { responseTexts } from "../../../lib/utils/constants"
 
 const getGradients = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "GET") {
-    const data = await colorGradient.find()
-    res.json(data)
+  if (req.method === "POST") {
+    // fetches all the color gradients from the DB
+    return getAllColorGradients()
+      .then((gradients) => {
+        res.status(200).json(gradients)
+      })
+      .catch(() => {
+        res.status(501).json(responseTexts.COULD_NOT_COMPLETE.MONGODB_FETCH)
+      })
+  } else {
+    res.status(403).send(responseTexts.TRY_DIFFERENT_REQUEST)
   }
 }
 
