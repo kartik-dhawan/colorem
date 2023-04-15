2e3bf1f259f6807ce7fe4bc9a8f8d04c68f9811f
kartik-dhawan <kartikkdhawan@gmail.com>
2023-04-15 14:26:52 +0530

    CW-203: Added custom favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="shortcut icon" href="/favicon/favicon.ico"></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
