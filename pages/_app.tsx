167dd83302e3b960d74b8d183ab75fb069b4bc58
kartik-dhawan <kartikkdhawan@gmail.com>
2023-06-15 00:10:27 +0530

    CW-235: Added progress bar for route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,27 @@
 import type { AppProps } from "next/app"
 import "../styles/index.scss"
 import Layout from "../components/common/Layout"
-import { useRouter } from "next/router"
+import Router, { useRouter } from "next/router"
 import store from "../redux/store"
 import { Provider } from "react-redux"
+import { useState } from "react"
+import { LinearProgress } from "@mui/material"
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
   const noLayoutPages = ["/_error", "/", "/404", "/about", "/login"]
+
+  const [routeLoading, setRouteLoading] = useState<boolean>(false)
+
+  Router.events.on("routeChangeStart", () => {
+    setRouteLoading(true)
+  })
+  Router.events.on("routeChangeComplete", () => {
+    setRouteLoading(false)
+  })
+  Router.events.on("routeChangeError", () => {
+    setRouteLoading(false)
+  })
 
   if (
     noLayoutPages.includes(router.pathname) ||
@@ -21,6 +35,7 @@ export default function App({ Component, pageProps }: AppProps) {
   } else {
     return (
       <Provider store={store}>
+        {routeLoading && <LinearProgress />}
         <Layout>
           <Component {...pageProps} />
         </Layout>
